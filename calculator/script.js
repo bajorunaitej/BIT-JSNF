0bc15082a5c27ad5f664053bee4648665356b688
Juste Bajorunaite <bajorunaitej@gmail.com>
2023-12-03 22:05:40 +0200

    if else

--- calculator/script.js
+++ calculator/script.js
@@ -22,21 +22,19 @@ function calculate(operation) {
 
     let selector = document.querySelector('#select').value;
     
-    const x = parseFloat(document.querySelector('x').value);
-    const y = parseFloat(document.querySelector('y').value);
+    const x = parseFloat(document.querySelector('#x').value);
+    const y = parseFloat(document.querySelector('#y').value);
 
 
     let result;
     if(selector === 'add') result = x + y;
     else if(selector === 'substract') result = x - y;
     else if(selector === 'multiply') result = x * y;
-    else if(selector === 'divide') {
-        if(y === 0) {
-            console.log('Dalyba iš nuolio negalima')
-        }else{result = x / y};
-    }
-    else if(selector === 'power') result = x ^ y;
-    else result = "Skaičiai neįvesti";
+    else if(selector === 'divide' && y === 0) result = 'Dalyba iš nulio negalima';
+    else if(selector === 'divide') result = x / y;
+    else if(selector === 'power') result = x ** y;
+    else result = '';
+
 
     document.querySelector('#result').innerText = `Atsakymas: ${result}`;
 }
