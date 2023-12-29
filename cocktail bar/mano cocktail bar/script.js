4d995255db165f4ececee0da60af2abc463716fd
Juste Bajorunaite <bajorunaitej@gmail.com>
2023-12-29 17:48:01 +0200

    modal

--- cocktail bar/mano cocktail bar/script.js
+++ cocktail bar/mano cocktail bar/script.js
@@ -1,11 +1,17 @@
 const cocktailNameFilterElement = document.querySelector('#cocktailName');
 const supriseMeButton = document.querySelector('#supriseMe');
+const modal = document.querySelector('#modal');
+const closeModal = document.querySelector('.close')
 
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
 .then((response) => response.json())
 .then((response) => console.log(response));
 
-supriseMeButton.addEventListener('click', () => {
-    
-})
+// supriseMeButton.addEventListener('click', () => {
+//     modal.show();
+// });
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
