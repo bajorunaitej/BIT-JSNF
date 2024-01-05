a82f961cda55da8957688c57f115872ced710ca1
Juste Bajorunaite <bajorunaitej@gmail.com>
2024-01-05 12:24:28 +0200

    Modal

--- cocktail bar/mano cocktail bar/script.js
+++ cocktail bar/mano cocktail bar/script.js
@@ -9,7 +9,7 @@ const modal = document.querySelector('#modalBg');
 
 const supriseMeButton = document.querySelector('#supriseMe');
 
-const selectValues = [],
+const selectValues = {},
     drinksArray = [];
 
 async function fillSelectElement() {
@@ -25,14 +25,12 @@ async function fillSelectElement() {
     const allValues = await Promise.all(allPromises);
     const [allCategories, allGlasses, allIngredients] = allValues;
 
-    // const [allCategories, allGlasses, allIngredients] = await Promise.all(allPromises);
-
     selectValues.categories = allCategories.drinks.map(categoryObj => categoryObj.strCategory);
     // console.log(selectValues.categories);
-    selectValues.glasses = allGlasses.drinks.map(glassObj => glassObj.strGlass);
-    selectValues.ingredients = allIngredients.drinks.map(ingredientObj => ingredientObj.strIngredient1);
+    selectValues.glasses = allGlasses.drinks.map((glass) => glass.strGlass);
+    selectValues.ingredients = allIngredients.drinks.map((ingredient) => ingredient.strIngredient1);
 
-    fillCategorySelect(allCategories.drinks, categorySelect, "strCategory");
+    fillCategorySelect(allCategories.drinks, categorySelectElement, "strCategory");
     fillCategorySelect(allGlasses.drinks, glassSelectName, "strGlass");
     fillCategorySelect(allIngredients.drinks, ingredientSelectName, "strIngredient1");
 }
@@ -46,16 +44,24 @@ function fillCategorySelect(properties, selectElement, strFieldName) {
 }
 
 async function getAllDrinks() {
+    const categoryDrinksUrls = [];
     for(const category of selectValues.categories) {
         // console.log(`${category} ←`);
         let dynamicUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(' ','_')}`;
-        const response = await fetch(dynamicUrl);
-        const answerFromServer = await response.json();
-
-        for(const drink of answerFromServer.drinks) {
-            drinksArray.push(drink);
-        }
+        categoryDrinksUrls.push(dynamicUrl);
     }
+    const allPromises = categoryDrinksUrls.map((url) =>
+        fetch(url).then((response) => response.json())
+    );
+    const allValues = await Promise.all(allPromises);
+    allValues.forEach((value) => drinksArray.push(...value.drinks));
+    //     const response = await fetch(dynamicUrl);
+    //     const answerFromServer = await response.json();
+
+    //     for(const drink of answerFromServer.drinks) {
+    //         drinksArray.push(drink);
+    //     }
+    // }
 }
 
 function generateDrinksHTML(drinks) {
@@ -72,11 +78,11 @@ function generateDrinksHTML(drinks) {
 
 async function filter() {
 	const searchValue = cocktailNameFilterElement.value,
-		category = categorySelectElement.value,
-		glass = glassSelectName.value,
-		ingredient = ingredientSelectName.value;
+            category = categorySelectElement.value,
+            glass = glassSelectName.value,
+            ingredient = ingredientSelectName.value;
 	let filteredArray = [...drinksArray];
-	console.log(filteredArray);
+	// console.log(filteredArray);
 
 	if (searchValue) {
 		filteredArray = filteredArray.filter((drinkObj) =>
@@ -97,7 +103,7 @@ async function filter() {
 			)
 		);
 	}
-	if (glass !== "Pasirinkite stiklinės tipą") {
+	if (glass !== "Stiklinės tipas") {
 		const promise = await fetch(
 			`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass.replaceAll(
 				" ",
@@ -111,13 +117,27 @@ async function filter() {
 			)
 		);
 	}
+	if (ingredient !== "Ingredientas") {
+		const promise = await fetch(
+			`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient.replaceAll(
+				" ",
+				"_"
+			)}`
+		);
+		const ingredientsOfDrink = await promise.json();
+		filteredArray = filteredArray.filter((drink) =>
+			ingredientsOfDrink.drinks.some(
+				(ingredientOgDrink) => drink.idDrink === ingredientOgDrink.idDrink
+			)
+		);
+	}
 	generateDrinksHTML(filteredArray);
 }
 
 async function initialisation() {
     // Select'ų užpildymas
     await fillSelectElement();
-    console.log(selectValues);
+    // console.log(selectValues);
     await getAllDrinks();
     generateDrinksHTML(drinksArray);
     buttoneSearch.addEventListener('click', filter);
@@ -130,8 +150,33 @@ async function openModal(id) {
     const response = await promise.json();
     const drink =  response.drinks[0];
     document.querySelector('.modalImg').src = drink.strDrinkThumb;
+    document.querySelector('.modalTitle').innerText = drink.strDrink;
     document.querySelector('#modalCategory').innerText = drink.strCategory;
-    document.querySelector('#modalAlcohol').innerText = drink.strAlcohol;
+    document.querySelector('#modalAlcohol').innerText = drink.strAlcoholic;
+    document.querySelector('#modalGlass').innerText = drink.strGlass;
+    document.querySelector('#modalRecipe').innerText = drink.strInstructions;
+    // ingredientų listas
+
+    let dynamicHTML = '';
+    for (let i = 1; i < 16; i++) {
+
+        const ingredientKey = `strIngredient${i}`;
+        const measureKey = `strMeasure${i}`;
+
+        const ingredient = drink[ingredientKey];
+        const measure = drink[measureKey];
+
+        if(ingredient !== null && ingredient !== undefined) {
+            console.log(ingredient);
+            console.log(measure);
+            // document.querySelector('.ingredient').innerText = ingredient;
+            // document.querySelector('#measure').innerText = measure;
+
+            dynamicHTML += `<p class="ingredient"><b>${ingredient}:</b> <span>${measure}</span></p>`;
+        }
+    document.querySelector('.ingredients').innerHTML = dynamicHTML;
+    }
+
 }
 
 function closeModal() {
