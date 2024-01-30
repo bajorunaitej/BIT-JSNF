86f4ec87b075aecb2c1176fa8e6b85df4db8dd95
Juste Bajorunaite <bajorunaitej@gmail.com>
2024-01-30 14:31:12 +0200

    app

--- imageApp/config/dbConnect.js
+++ imageApp/config/dbConnect.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 //Laikinai sukonfiguruoja .env kintamuosius, kad jie būtų matomi mūsų kūrimo aplikose
 require("dotenv").config();
 
+const mongoUrl = process.env.MONGO_CONNECTION.replace(
+	"__DB_USER",
+	process.env.DB_USER
+)
+	.replace("__DB_PASSWORD", process.env.DB_PASSWORD)
+	.replace("__DB_HOST", process.env.DB_HOST)
+	.replace("__DB_NAME", process.env.DB_NAME);
 
 function config() {
     //process.env - laiko visus aplikos kintamuosius
     // console.log(process.env.MONGO_CONNECTION.replace('__DB_USER', process.env.DB_USER).replace('__DB_PASSWORD', process.env.DB_PASSWORD).replace('__DB_HOST', process.env.DB_HOST).replace('__DB_NAME', process.env.DB_NAME));
 
     //Prisijungimas prie duomenų bazės pasinaudojant URL
-    mongoose.connect(
-        process.env.MONGO_CONNECTION.replace('__DB_USER', process.env.DB_USER)
-            .replace('__DB_PASSWORD', process.env.DB_PASSWORD)
-            .replace('__DB_HOST', process.env.DB_HOST)
-            .replace('__DB_NAME', process.env.DB_NAME));
+    mongoose.connect(mongoUrl);
     const db = mongoose.connection;
 
     //DB listeneriai, kurie nusako, ar prie DB buvo prisijungta sėkmingai ar ne
@@ -26,5 +29,5 @@ function config() {
 }
 
 //su objektu eksportuoja daugiau funkcijų
-module.exports = {config};
+module.exports = {config, mongoUrl};
 // module.exports = config;
--- imageApp/config/expressMiddlewares.js
+++ imageApp/config/expressMiddlewares.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const pagesRouter = require('../routes/pages')
-const userRouter = require('../routes/userRouter')
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const pagesRouter = require('../routes/pages');
+const userRouter = require('../routes/userRouter');
+const bodyParser = require("body-parser");
 
 function config(app) {
     //Nustatymas EJS aktyvavimui (middleware)
@@ -14,12 +17,30 @@ function config(app) {
 
     //Middleware - skirtas gauti JSON formato duomenis iš kliento
     app.use(express.json())
+    app.use(bodyParser.urlencoded());
+    //Sesijų nustatymai
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false,
+            //Sesijų saugojimui duomenų bazėje
+            store: MongoStore.create({
+                mongoUrl: require("./dbConnect").mongoUrl, //mongodb+srv://__DB_USER:__DB_PASSWORD@__DB_HOST/__DB_NAME
+                collectionName: "sessions",
+            }),
+            cookie: {
+                maxAge: 1000*60*60*24*7,
+            },
+        })
+    );
 
     //Tarpinio route panaudojimas, pasiekiamas per http://localhost/public endpoint'ą
     app.use("/public", publicRouter);
 
     //Puslapių route'ai:
     app.use(pagesRouter);
+    
     app.use('/api/user', userRouter);
     // localhost:3000/api/user/userRouter
 
