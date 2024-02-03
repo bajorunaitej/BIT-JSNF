7084f438cb45f533f860bcfeb1837c233a874c67
Juste Bajorunaite <bajorunaitej@gmail.com>
2024-02-03 19:43:48 +0200

    dialog

--- netflix/script.js
+++ netflix/script.js
@@ -5,3 +5,18 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     })
 });
+
+const signInBtn = document.querySelector('#signin-btn');
+const closeBtn = document.querySelector('#close-btn');
+
+signInBtn.addEventListener('click', () => {
+    document.querySelector('.main-page').style.display = "none";
+    document.querySelector('.favDialog').style.display = "grid";
+    document.querySelector('.favDialog').style.gridTemplateColumn = "1fr";
+});
+
+closeBtn.addEventListener('click', () => {
+    document.querySelector('.main-page').style.display = "block";
+    document.querySelector('.favDialog').style.display = "none";
+
+})
