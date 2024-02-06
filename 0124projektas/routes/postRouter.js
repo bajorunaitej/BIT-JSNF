a2dc8c3549f53a2d73d888eecad2ce576776dee3
Juste Bajorunaite <bajorunaitej@gmail.com>
2024-02-06 07:59:15 +0200

    /

--- 0124projektas/routes/postRouter.js
+++ 0124projektas/routes/postRouter.js
@@ -38,20 +38,20 @@ router.delete("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-	//Naujo įrašo sukūrimas
-	const {title, content} = req.body;
+	const { title, content } = req.body;
 	const authorId = req.session.user.id;
 
-	//Validacija
-	
+	// Validacija
 
-	//Išsaugojimas db
+	// Išsaugojimas duombazėje
 	const newPost = new PostModel({
-		title, content, authorId
+		title,
+		content,
+		authorId,
 	});
-	await newPost.save();
-	res.redirect('/message=Naujas postas buvo sėkmingai sukurtas');
 
+	await newPost.save();
+	res.redirect("/?message=Naujas postas buvo sėkmingai sukurtas");
 });
 
 router.put("/", async (req, res) => {
