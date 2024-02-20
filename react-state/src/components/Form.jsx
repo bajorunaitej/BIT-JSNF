15c00f8a13f7081c178bea27847b76cc715d19ca
Juste Bajorunaite <bajorunaitej@gmail.com>
2024-02-20 19:50:57 +0200

    ff

--- react-state/src/components/Form.jsx
+++ react-state/src/components/Form.jsx
@@ -1,20 +1,22 @@
 // Sukurti komponentą su dviem range tipo įvedimais https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range vienu color įvedimu https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color ir mygtukais sukurti ir išsaugoti. Paspaudus mygtuką sukurti, atsiranda naujas kvadratas 100px aukščio ir pločio bei juodu fonu. Keičiant range ir color įvedimus keičiasi ir kvadrato išvaizda. Kvadrato išvaizdą nustato įvedimai: range tipo įvedimai nuo 10 iki 200 ir nustato plotį ir aukštį pikseliais, color - fono spalvą. Paspaudus mygtuką išsaugoti, kvadrato išvaizda išsaugoma ir į nustatymus nebereguoja. Vėl paspaudus mygtuką sukurti- atsiranda naujas reguliuojamas kvadratas.
 
-export default function Form5() {
+import { useState } from "react";
+
+export default function Form() {
   return (
     <div>
-      <div>
+      <div className="flex justify-evenly items-center">
         <input type="range" name="height" min={10} max={200} />
         <label htmlFor="height">Height</label>
-        <br />
+
         <input type="range" name="width" min={10} max={200} />
         <label htmlFor="width">Height</label>
-        <br />
+
         <input type="color" name="color" value="#000" />
         <label htmlfor="color">Color</label>
-        <br />
+      </div>
+      <div className="flex gap-2">
         <button>Create</button>
-        <br />
         <button>Save</button>
       </div>
     </div>
--- react-state/src/index.js
+++ react-state/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Section from "./components/Section";
-import FormSection from "./components/FormSection";
+// import Section from "./components/Section";
+// import FormSection from "./components/FormSection";
+import Form from "./components/Form";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FormSection />
-    <Section />
+    {/* <FormSection /> */}
+    {/* <Section /> */}
+    <Form />
   </React.StrictMode>
 );
